9da3f19294b39a89d0edfb45146e797e4e36d3d7
adityaexe <kmradi2002@gmail.com>
2024-05-14 21:22:29 +0530

    payroll system class added

--- Employee Payroll System/src/Main.java
+++ Employee Payroll System/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 abstract class Employee {
 
     private String name;
@@ -54,6 +56,43 @@ class partTimeEmployee extends Employee {
     public double calculateSalary() {
         return hourlyRate * hoursWork;
     }
+}
+
+class PayrollSystem {
+
+    private ArrayList<Employee> employeeList;
+
+    public PayrollSystem() {
+        employeeList = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public void removeEmployee(int id) {
+
+        Employee employeeToRemove = null;
+
+        for (Employee employee : employeeList) {
+            if (employee.getId() == id) {
+                employeeToRemove = employee;
+                break;
+            }
+        }
+
+        if (employeeToRemove != null) {
+            employeeList.remove(employeeToRemove);
+        }
+
+    }
+
+    public void displayEmployee() {
+        for (Employee employee : employeeList) {
+            System.out.println(employee);
+        }
+    }
+
 }
 
 public class Main {
